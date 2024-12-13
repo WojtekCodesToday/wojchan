615fa13bf7f372d4dc2927595dc35d44699c07c5
WojtekGame <94251112+WojtekCodesToday@users.noreply.github.com>
2024-12-13 19:04:28 +0100

    sprite.png change and added comments
    since jsdelivr already got the spritesheet i can just replace the gh link
    added comments if the code is too cluttered, won't explain everything that is in it though :(

--- wojchan.js
+++ wojchan.js
@@ -1,43 +1,60 @@
+//wojtekgame 2024
 (function wojchan() {
-  const c = document.createElement('canvas');
-  c.width = c.height = 61;
-  c.style.position = 'fixed';
-  c.style.pointerEvents = 'none';
-  c.style.zIndex = String(Number.MAX_VALUE);
-  c.style.imageRendering = "pixelated";
-  document.body.appendChild(c);
-  const ctx = c.getContext('2d');
+  const canvas = document.createElement('canvas');
+  const grid = 30;
+  canvas.id="wojchan"
+  //for SOME reason the pet sprite is slightly weird and you can see this when you zoom in, so i added +1
+  //for zIndex, it should be ontop of everything in the site, hopefully.
+  canvas.width = canvas.height = 61;
+  canvas.style.position = 'fixed';
+  canvas.style.pointerEvents = 'none';
+  canvas.style.zIndex = String(Number.MAX_VALUE);
+  canvas.style.imageRendering = "pixelated";
+  //adds itself to the page, opens a context to render simple 2d graphics,
+  //nothing special if used canvas before,
+  //and disabling imageSmoothingEnabled should make the canvas no longer blurry when scaled twice (30x30 => 60x60)
+  //the pet should be at the CENTER of the page (canvas_pos), (mouse) copies the same position.
+  //YOU CAN CHANGE THE SRC TO ANYTHING AS LONG AS IT FITS IN AN 90x90 SPRITESHEET :D
+  // (here using the one from the gh repository)
+  //rend() uses the spritesheet, one grid is 30x30 pixels and this renders one of them.
+  //then it should present the moving animation
+  document.body.appendChild(canvas);
+  const ctx = canvas.getContext('2d');
   ctx.imageSmoothingEnabled=false;
-  let m = [];
-  let cs = [window.innerWidth / 2, window.innerHeight / 2];
-  let state = 0;
-  window.addEventListener('mousemove', (e) => { m = [e.clientX, e.clientY] });
-  const image = new Image(180, 120);
-  image.src = "https://raw.githubusercontent.com/WojtekCodesToday/wojchan/refs/heads/master/sprite.png";
-  const rend = (x, y) => { ctx.drawImage(image, x * 30, y * 30, 30, 30, 0, 0, 30, 30) }
+  let canvas_pos = {x:window.innerWidth / 2, y:window.innerHeight / 2};
+  let mouse = {...canvas_pos};
+  window.addEventListener('mousemove', (e) => {mouse.x=e.clientX;mouse.y=e.clientY});
+  const image = new Image(grid*3, grid*3);
+  image.src = "https://cdn.jsdelivr.net/gh/wojtekcodestoday/wojchan@master/sprite.png";
+  const rend = (x, y) => { ctx.drawImage(image, x * grid, y * grid, grid, grid, 0, 0, grid, grid) }
   let mv = 0;
-  function animate() {
-    const dx = m[0] - cs[0];
-    const dy = m[1] - cs[1];
+
+  //anim() makes the animation render as well as "walking" to the cursor
+  const anim=()=>{
+    const dx = mouse.x - canvas_pos.x;
+    const dy = mouse.y - canvas_pos.y;
     const dist = Math.sqrt(dx * dx + dy * dy);
     const angle = Math.atan2(dy, dx);
     const flip=Math.cos(angle) < 0;
-    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.clearRect(0, 0, 61,61);
     ctx.setTransform(1, 0, 0, 1, 0, 0);
     ctx.scale(2,2)
     let spd = dist > 150 ? 8 : 4;
-    c.style.left = cs[0] - 30 + "px";
-    c.style.top = cs[1] - 30 + "px";
+    canvas.style.left = canvas_pos.x - 30 + "px";
+    canvas.style.top = canvas_pos.y - 30 + "px";
     rend(0, 2);
     mv = (mv + 1) % 4;
-    state == 0 ? rend(mv == 3 ? 1 : mv, flip) : rend(1, 0)
+    //walking or idling, (mv) is the walking cycle, it's third frame (4 in total),
+    //it gets replaced by the idle animation (makes the animation more alive)
+    dist > 15 ? rend(mv == 3 || mv, flip) : rend(1, 0)
     dist > 150 && rend(!flip+1, 2)
-    state = dist > 15 ? 0 : 1;
+    //goes to (speed) at the direction of (angle)
     if (dist > 15) {
-      cs[0] += Math.cos(angle) * spd;
-      cs[1] += Math.sin(angle) * spd;
+      canvas_pos.x += Math.cos(angle) * spd;
+      canvas_pos.y += Math.sin(angle) * spd;
     }
-    setTimeout(() => { requestAnimationFrame(animate) }, 70);
+    //redo animation again in 70ms, just so it's not too slow but also not too fast
+    setTimeout(() => requestAnimationFrame(anim), 70);
   }
-  image.onload = animate();
+  image.onload = anim();
 })();
